56623fa956b41ad98d29cf28a333d18c096cecbf
Benjamin Pearson <benjamin@developinginthedark.com>
2015-06-17 08:51:57 +1000

    Added first example mocha tests
    Using an example library that performs simply sum and multiplication in
    order to create an example test using mocha.

---
+++ test/example.js
@@ -0,0 +1,19 @@
+/**
+ * Example test script
+ */
+
+var example = require('../lib/example');
+var should = require('should');
+
+describe('Example', function() {
+	describe('#sum', function() {
+		it('should calculate the sum of two values', function(done) {
+			example.sum(2, 2).should.eql(4);
+			done();
+		});
+		it('should calculate the multiplication of two values', function(done) {
+			example.multiply(4, 2).should.eql(8);
+			done();
+		});
+	});
+});
