9ae7ced896ccd959a024beabc99d9e3e6f071186
Benjamin Pearson <bj.pearson@gmail.com>
2019-09-23 11:12:40 +1000

    Add nextjs

--- api/graphql.js
+++ api/graphql.js
@@ -1,17 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server-micro");
 
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
   }
@@ -38,7 +31,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  playground: true
+  playground: true,
+  bodyParser: true
 });
 
-module.exports = server.createHandler({ path: "/api" });
+module.exports = server.createHandler({ path: "/api/graphql" });
